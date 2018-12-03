f8581d176747d5eeaffc2ce459aff12dca2d8487
86176 <tyzy668@163.com>
2018-12-03 18:34:04 +0800

    execel上传功能开发实例

---
+++ app/Http/Controllers/ExeclController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Exports\UsersExport;
+use App\Imports\UsersImport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ExeclController extends Controller
+{
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function importExportView()
+    {
+        return view('import');
+    }
+
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function export()
+    {
+        return Excel::download(new UsersExport, 'users.xlsx');
+    }
+
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function import()
+    {
+        Excel::import(new UsersImport,request()->file('file'));
+
+        return back();
+    }
+}
---
+++ resources/views/import.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 5.7 Import Export Excel to database Example - ItSolutionStuff.com</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+</head>
+<body>
+
+<div class="container">
+    <div class="card bg-light mt-3">
+        <div class="card-header">
+            Laravel 5.7 Import Export Excel to database Example - ItSolutionStuff.com
+        </div>
+        <div class="card-body">
+            <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <input type="file" name="file" class="form-control">
+                <br>
+                <button class="btn btn-success">Import User Data</button>
+                <a class="btn btn-warning" href="{{ route('export') }}">Export User Data</a>
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
